0df98f0a3e45ac66e86f91c035b343c355436d33
shadowoflobster <68239409+shadowoflobster@users.noreply.github.com>
2024-09-12 18:26:16 +0400

    Added Card and Header components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Card from './components/Card/Card';
 function App() {
   return (
+    <>
     <Header></Header>
+    <Card></Card>
+    </>
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import 'Card.css';
+import bedRoomsSvg from "../SVGs/bedRoomsSvg.svg";
+import areaIcon from "../SVGs/areaIcon.svg";
+import locationIcon from "../SVGs/locationIcon.svg";
+import zipIcon from "../SVGs/zipIcon.svg";
+import './Card.css';
 
-const Card = () =>{
-    return(
-        <div className="cardDIv">
-            <img className="cardImage"></img>
-            <div className="cardDetails"></div>
+const Card = () => {
+    return (
+        <div className="cardDiv">
+            <div className="cardImage"><div className="offerStatus">იყიდება</div></div>
+            <div className="cardDetailsDiv">
+                <div className="cardDetails">
+                    <span className="cardPrice">80 000 ₾</span>
+                    <div className="cardAdress"><img className="locationSvg"  src={locationIcon}></img><span className="cardAdressSpan">თბილისი, ი. ჭავჭავაძის 53</span></div>
+                    <div className="cardIconDetails">
+                        <div className="bedRooms"><img className="bedRoomsSvg" src={bedRoomsSvg}></img><span className="bedRoomsValue">2</span></div>
+                        <div className="area"><img className="areaIcon" src={areaIcon}></img><span className="areaValue">55 მ</span><sup>2</sup></div>
+                        
+                        <div className="zip"><img className="zipIcon" src={zipIcon}></img><span className="zipValue">2</span></div>
+
+                    
+                </div>
+            </div>
         </div>
+</div>
     )
 }
+
+export default Card
